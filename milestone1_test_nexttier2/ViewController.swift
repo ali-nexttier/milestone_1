5a07993f79c7db7c8ab3729470c5b2ca016d6e4f
Ali Syed <4limsyed@gmail.com>
2022-09-09 18:09:55 +0500

    first commit

---
+++ milestone1_test_nexttier2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  milestone1_test_nexttier2
+//
+//  Created by ali on 9/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
